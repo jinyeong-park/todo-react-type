c4947baaea7bdd04209798b9ca5a60b9b3133275
Jinyeong Park <jinyeongpark1205@gmail.com>
2021-04-06 17:58:30 -0700

    create toggletodo function

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -14,12 +14,28 @@ const initialTodos: Array<Todo> = [
 ];
 
 const App: React.FC = () => {
+  // state
   const [todos, setTodos] = useState(initialTodos);
+
+  // funtion to toggle todo
+  const toggleTodo = (selectedTodo: Todo) => {
+    const newTodos = todos.map(todo => {
+      if (todo === selectedTodo) {
+        return {
+          ...todo,
+          complete: !todo.complete
+        }
+      }
+      // if not
+      return todo;
+    });
+    setTodos(newTodos);
+  }
 
   return (
     <React.Fragment>
-      <ToDoListItem todo={todos[0]}/>
-      <ToDoListItem todo={todos[1]}/>
+      <ToDoListItem todo={todos[0]} toggleTodo={toggleTodo}/>
+      <ToDoListItem todo={todos[1]} toggleTodo={toggleTodo}/>
     </React.Fragment>
 
   )
